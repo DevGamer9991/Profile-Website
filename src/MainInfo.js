c768ed95f37defb1ffa6aa22064055ed8d8c4f94
DevGamer9991 <pathornton114@gmail.com>
2022-01-21 16:41:50 -0600

    Update

--- src/MainInfo.js
+++ src/MainInfo.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import BlobBG from "./graphics/BlobBG.svg";
+
+var Info = ["Programmer", "Developer", "IT Profesional"];
 
 const Main = styled.div`
     display: flex;
@@ -22,11 +24,14 @@ const MainTextWrapper = styled.div`
 `;
 
 const MainText = styled.h1`
-    font-size: 60px;
+    font-size: 65px;
     font-weight: normal;
+    margin: 0;
+    margin-bottom: 15px;
+    /* background: red; */
 
     strong {
-        font-size: 65px;
+        font-size: 75px;
         background: linear-gradient(to right, var(--primary), var(--secondary));
         -webkit-background-clip: text;
         background-clip: text;
@@ -35,23 +40,26 @@ const MainText = styled.h1`
 `;
 
 const SecondaryText = styled.p`
+    font-size: 20px;
+    margin: 0;
+    margin-top: 10px;
+    padding-left: 5px;
+
+    /* background: red; */
     strong {
         color: var(--primary)
     }
 `;
 
-const Blinker = styled.div`
-    width: 2px;
-    height: 10px;
-    background: red;
-`;
-
 const MainInfo = () => {
+
+    var value = Info[Math.floor(Math.random() * Info.length)];
+
     return (
         <Main>
             <MainTextWrapper>
-                <MainText>Hi, My Name is <br></br> <strong>Parker Thornton</strong></MainText>
-                <SecondaryText>And I Am a <br></br><strong>Programmer</strong></SecondaryText><Blinker />
+                <MainText>Hi, My Name is <br></br><strong>Parker Thornton</strong></MainText>
+                <SecondaryText>And I Am a Self Taught <strong>Programmer</strong> and <strong>Software Developer</strong></SecondaryText>
             </MainTextWrapper>
             <Blob src={BlobBG} />
         </Main>
