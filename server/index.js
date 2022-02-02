d994ba9dc2fa701663486b1826fc691fb9598c73
DevGamer9991 <pathornton114@gmail.com>
2022-02-01 20:00:00 -0600

    Update

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+
+var port = 5010;
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'Site')));
+
+app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, 'Site/index.html'));
+})
+
+console.log(`Listening on port ${port}`)
+app.listen(port);
