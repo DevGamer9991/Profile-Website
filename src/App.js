f55e7bc1e5bcf7e89941037553829b7cba3b0baa
Parker Thornton <pathornton114@gmail.com>
2023-05-30 01:24:47 -0500

    Update Items List And Index

--- src/App.js
+++ src/App.js
@@ -19,19 +19,19 @@ const ItemWrapper = styled.div`
 
 function App() {
 
-  const [items, setItems] = useState([
+  const items = [
     { key: 1, cssId: "about-me", title: "About Me", desc: "Hi, I'm Parker Thornton. Since the age of 8, I've been knee-deep in coding and I've spent the better part of my life as a dedicated software developer. For the last 4 years, I've branched out into video editing and production, finding unique ways to blend my tech skills with creative output. Just shy of 18, I'm excited for what the future holds.", imageText: "</>" },
-  ]);
+  ];
 
   return (
     <Main>
       <Navbar items={items}/>
       <MainInfo />
       <ItemWrapper>
-        {items.map((item) => (
+        {items.map((item, i) => (
             <>
               {console.log(item.cssId)}
-              <Item itemID={item.cssId} key={item.key} {...item} />
+              <Item itemID={item.cssId} key={i} {...item} />
             </>
         ))}
         <Projects username="DevGamer9991"/>
